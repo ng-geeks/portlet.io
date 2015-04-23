d0bccd6be96f9988ce55bc3c2b7240e2f1131a98
netherworks <leonkiley@watersedgeweb.com>
2015-04-23 13:09:19 -0400

    initial commit

---
+++ portlet.js
@@ -0,0 +1,36 @@
+(function() {
+    'use strict';
+
+    var  portlet = function() {
+        return {
+            restrict: 'E',
+            templateUrl: './views/directives/portlet.html',
+            transclude: true,
+            scope: {
+                title : '@portletTitle',
+                caption : '@portletCaption',
+                actions : '@actionsTemplate',
+                tabs : '@tabsTemplate',
+                titleClass : '@',
+                icon : '@portletIcon',
+                vm : '=viewModel',
+                includeBottomActions : '=includeBottomActions',
+                bottomDynamicButtons : '=bottomDynamicButtons',
+                bottomButtonsOffset : '@bottomButtonsOffset',
+                showFooterSpinner : '=showFooterSpinner',
+                showSpinner : '=showSpinner',
+                dropdownData : '=dropdownData',
+                dynamicButtons : '=dynamicButtons',
+                dynamicActions : '=dynamicActions'
+            },
+            link: function(scope, element, attrs) {
+                scope.onActionItemClicked = function(item) { // Click handler for dynamic items
+                    item.callback();
+                };
+            }
+        };
+    };
+
+    angular.module('app')
+        .directive('portlet', portlet);
+}());
