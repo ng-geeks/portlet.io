a6eefbe01d0b46a208a9a3400326536a00bbdca4
Scott Engemann <engemasa@gmail.com>
2016-03-01 21:32:19 -0500

    Building demo page and adding demo data

--- dynamic-table/dynamicTable.js
+++ dynamic-table/dynamicTable.js
@@ -1,37 +1,47 @@
 (function () {
     'use strict';
-    angular.module('myApp').directive('dynamicTable', function($log) {
+    angular.module('portlet.io.dynamictable', []).controller('dynamicTableController', function ($scope, $log) {
+        var vm = this;
+        vm.onRowClicked = function (callback) {
+            try {
+                callback(this.row);
+            } catch (error) {
+                $log.error(error);
+            }
+        };
+    }).directive('dynamicTable', function ($log) {
         return {
-            restrict: 'EA',
-            templateUrl: './views/directives/dynamic-table.html',
+            restrict: 'E',
             transclude: true,
             scope: {
-                scrollBody: '=scrollBody',
-                data: '=tableData',
-                vm: '=viewModel'
+                scrollBody : '=',
+                data : '=tableData',
+                sortBy : '='
             },
-            link: function(scope, element, attrs) {
-                scope.onRowClicked = function(callback) { // Click handler rows
+            //controller : 'dynamicTableController',
+            //controllerAs : 'ctrl',
+            templateUrl : '../dynamic-table.html',
+            link : function (scope, element, attrs) {
+                scope.onRowClicked = function (callback) {
                     try {
                         callback(this.row);
-                    }
-                    catch(error) {
+                    } catch (error) {
                         $log.error(error);
                     }
-                }
+                };
             }
-        }
+        };
     });
-    /*
-    *   --- DATA EXAMPLE ---
-    *   vm.lookupResults = {columns : [], rows: [], keys: [], rowClickHandler : yourScopeControllerFunction};
-    *   vm.lookupResults.rows = data.DATA.ROWS;
-    *   vm.lookupResults.columns = _.map(_.sortBy(data.COLUMNS, function(item) {
-    *       if(item.LABEL !== undefined) {
-    *           vm.lookupResults.keys.push(item.ID);
-    *       }
-    *       return item.ORDER;
-    *   }), "LABEL");
-    *
-    * */
+    /* -- DATA EXAMPLE --
+    
+    vm.lookupResults = {columns : [], rows: [], keys: [], rowClickHandler : yourScopeControllerFunction};
+    vm.lookupResults.rows = data.DATA.ROWS;
+    vm.lookupResults.columns = _.map(_.sortBy(data.COLUMNS, function(item) {
+        if(item.LABEL !== undefined) {
+            vm.lookupResults.keys.push(item.ID);
+        }
+        return item.ORDER;
+    }), "LABEL");
+    
+    */
 }());
